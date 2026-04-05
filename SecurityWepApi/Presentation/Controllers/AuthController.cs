b6204947aef5fda6d07ad75c5b4e9c79be878e9b
loricode <loritec26@gmail.com>
2026-04-04 22:47:05 -0500

    comienzo del proyecto

---
+++ SecurityWepApi/Presentation/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityWebApi.Controllers {
+
+[ApiController]
+[Route("[controller]")]
+public class ParkingController: ControllerBase
+    {
+        
+    }
+}
---
+++ SecurityWepApi/Program.cs
@@ -0,0 +1,37 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors("CorsPolicy");
+
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
